c8bfff56cbd3263d55760c69c2086cb1af5e9b1f
KateC08 <coming.katelyn08@gmail.com>
2024-09-26 15:19:13 +0800

    added Conditional Statements

--- app.js
+++ app.js
@@ -13,3 +13,32 @@ let res = x / y;
 let result = number * 5 + 2;
 console.log(result);
 let isTrue = number > 5 && boolean;
+
+// Conditional Statements
+// if 
+let wer = 2;
+if (wer > 5) {
+    console.log("Were na you?");
+}
+//if else
+if (number % 2 === 0) {
+    console.log("Number is even");
+} else {
+    console.log("Number is odd");
+}
+
+// switch
+let day = 1;
+switch (day) {
+    case 1:
+        console.log("It's Day 1");
+        break;
+    case 2:
+        console.log("It's Day 2");
+        break;
+    case 3:
+        console.log("It's Day 3");
+        break;    
+    default:
+        console.log("It's another day for taking JS");
+}
