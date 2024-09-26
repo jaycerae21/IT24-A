c50f98cb441b0a208a4f7e081be30f86728fe87f
KateC08 <coming.katelyn08@gmail.com>
2024-09-26 15:17:19 +0800

    added arithmetic and logical operators

--- app.js
+++ app.js
@@ -4,3 +4,12 @@ let  nicknaeme = "hello";
 let isinCorrect = true;
 let katulgon;
 let katulgons = null;
+
+// Arithmetic and Logical Operators
+
+let x = 2;
+let y = 3;
+let res = x / y;
+let result = number * 5 + 2;
+console.log(result);
+let isTrue = number > 5 && boolean;
