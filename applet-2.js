7be040af769d29f8904458c6722ba312e080ee96
jjjjustine <catingan.janejustine@gmail.com>
2024-10-04 14:37:22 +0800

    added const myMap

--- applet-2.js
+++ applet-2.js
@@ -11,10 +11,11 @@ class LeafletMap {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(this.map);
     }
-}
+
 addMarker(lat, lng, message) {
     const marker = L.marker([lat, lng]).addTo(this.map);
     marker.bindPopup(message)
+}
 
     loadMarkersFromJson(url) {
         fetch(url)
@@ -27,3 +28,13 @@ addMarker(lat, lng, message) {
             .catch(error => console.error('Error loading markers:', error));
     }
 }
+
+const myMap = new LeafletMap('map', [8.360004, 124.868419], 18);
+
+/*
+myMap.addMarker(8.359735, 124.869206, 'CCS Faculty Office');
+myMap.addMarker(8.359639,124.869179, 'CCS Laboratory 1');
+myMap.addMarker(8.359554,124.869153, 'CCS Laboratory 2');
+*/
+
+myMap.loadMarkersFromJson('applet-2.json');
