5ad1f23b3efaac096989860cae78b81d1252090f
jaycerae21 <20231641@nbsc.edu.ph>
2024-10-17 10:24:34 +0800

    added object

--- applet-5.js
+++ applet-5.js
@@ -100,6 +100,7 @@ class WeatherService extends WeatherApp {
 
     async getWeatherDataByCoordinates(latitude, longitude) {
         try {
+
             const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${this.apiKey}&units=metric`);
             if (response.ok) {
                 return await response.json();
@@ -109,7 +110,7 @@ class WeatherService extends WeatherApp {
         }
         return null;
     }
-    
+
 
 }
 
