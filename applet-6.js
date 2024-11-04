15d9b3277897e60737e4508f3f5d5ed781f383cd
jaycerae21 <20231641@nbsc.edu.ph>
2024-11-04 14:01:48 +0800

    added update task

--- applet-6.js
+++ applet-6.js
@@ -52,13 +52,14 @@ class todoList{
         const buttons = taskItem.querySelectorAll('button');
         buttons.forEach(button => button.disabled = true);
     }
-    
+
 
 
     updateTask(taskText) {
         this.todoList.children[this.editingIndex].querySelector('.task-text').textContent = taskText;
         this.resetEditing();
     }
+    
     
 
     removeTask(event) {
